fab7a8d20cbee9e4cdce417d40136fcb89e0c24d
n4inz <zulkar.n4inz@gmail.com>
2024-06-27 13:21:59 +0800

    deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,36 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
 
-export default nextConfig;
+// export default nextConfig;
+
+
+// next.config.mjs
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    // output: 'export',
+    eslint: {
+      // Warning: This allows production builds to successfully complete even if
+      // your project has ESLint errors.
+      ignoreDuringBuilds: true,
+    },
+    experimental: {
+      missingSuspenseWithCSRBailout: false,
+    },
+  
+    async rewrites() {
+      return [
+        {
+          source: '/',
+          destination: '/',
+        },
+        {
+          source: '/logout',
+          destination: '/',
+        },
+      ];
+    },
+  
+  };
+  
+  export default nextConfig;
