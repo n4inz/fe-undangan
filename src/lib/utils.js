f6133d6632b9096cd53439035f2ac99c28563b74
try333 <triumpar@gmail.com>
2025-02-24 17:46:34 +0800

    fix: resolve merge conflicts and clean up bank-related code in admin detail pages

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
 import { clsx } from "clsx";
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs) {
   return twMerge(clsx(inputs));
-=======
-import { clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
-
-export function cn(...inputs) {
-  return twMerge(clsx(inputs))
->>>>>>> 735029a64416b8d20fa969c1ddac70dfc49988b4
 }
